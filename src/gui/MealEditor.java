ca2cd3979f4d90135d688f24ea2c4d637aeb2fc6
Josiah Leach <leachjr@dukes.jmu.edu>
2023-03-29 10:42:30 -0400

    Added changed MealEditor to extend JDialog and added a constructor.

--- src/gui/MealEditor.java
+++ src/gui/MealEditor.java
@@ -1,6 +1,25 @@
 package gui;
 
-public class MealEditor
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class MealEditor extends JDialog
 {
 
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  
+  /**
+   * Creates a new MealEditor.
+   * @param owner The JFrame which created this MealEditor. This should probably be
+   * the Main Window.
+   */
+  public MealEditor(JFrame owner)
+  {
+    
+  }
+
+  
 }
