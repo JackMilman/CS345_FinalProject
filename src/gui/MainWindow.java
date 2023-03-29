1950ed61027c90dd3756e133a8de3adcea3a52a3
Shelsey Vega <vega2sx@dukes.jmu.edu>
2023-03-29 10:01:10 -0400

    Add the menu bar to the window.

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,6 +1,72 @@
 package gui;
 
-public class MainWindow
+import javax.swing.*;
+
+public class MainWindow extends JFrame
 {
-  
+  private static final long serialVersionUID = 1L;
+
+  public MainWindow()
+  {
+    // set the size of the frame
+    setSize(500, 500);
+    setVisible(true);
+
+    // create a menu bar and add the items
+    JMenuBar menuBar = new JMenuBar();
+    setJMenuBar(menuBar);
+
+    JMenu File = new JMenu("File");
+    menuBar.add(File);
+    JMenuItem Exit = new JMenuItem("Exit");
+    File.add(Exit);
+
+    JMenu Edit = new JMenu("Edit");
+    menuBar.add(Edit);
+    JMenuItem Recipe = new JMenuItem("Recipe");
+    File.add(Recipe);
+    JMenuItem Meal = new JMenuItem("Meal");
+    File.add(Meal);
+
+    JMenu Search = new JMenu("Search");
+    menuBar.add(Search);
+    JMenuItem Recipes = new JMenuItem("Recipes");
+    File.add(Recipes);
+    JMenuItem Meals = new JMenuItem("Meals");
+    File.add(Meals);
+
+    JMenu View = new JMenu("View");
+    menuBar.add(View);
+    JMenuItem shoppingList = new JMenuItem("ShoppingList");
+    File.add(shoppingList);
+    JMenuItem Process = new JMenuItem("Process");
+    File.add(Process);
+
+    JMenu Tools = new JMenu("Tools");
+    menuBar.add(Tools);
+    JMenuItem calorieCalculator = new JMenuItem("Calorie Calculator");
+    File.add(calorieCalculator);
+    JMenuItem unitsConverter = new JMenuItem("Units Converter");
+    File.add(unitsConverter);
+
+    JMenu Configure = new JMenu("Configure");
+    menuBar.add(Configure);
+    JMenuItem Preferences = new JMenuItem("Preferencesr");
+    File.add(Preferences);
+    JMenuItem Shortcuts = new JMenuItem("Shortcuts");
+    File.add(Shortcuts);
+    JMenuItem Nutrition = new JMenuItem("Nutrition");
+    File.add(Nutrition);
+
+    JMenu Help = new JMenu("Help");
+    menuBar.add(Help);
+    JMenuItem About = new JMenuItem("About");
+    File.add(About);
+    JMenuItem userGuide = new JMenuItem("User Guide");
+    File.add(userGuide);
+    
+    // add the company logo to the window
+    
+  }
+
 }
