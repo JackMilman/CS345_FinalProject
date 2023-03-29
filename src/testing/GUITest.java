8e5e7ccf3b77503fe96cb28763348bcf98c104d1
mearapatterson <patte2mk@dukes.jmu.edu>
2023-03-29 10:59:43 -0400

    Added ShoppingListViewer (unfinished), and a class to informally test GUI components.

---
+++ src/testing/GUITest.java
@@ -0,0 +1,45 @@
+package testing;
+
+import java.util.ArrayList;
+
+import gui.ShoppingListViewer;
+import recipes.Ingredient;
+import recipes.Recipe;
+import recipes.Step;
+import recipes.Utensil;
+
+/**
+ * Informal class to test if GUI works properly (not using JUnit).
+ * Can be deleted later.
+ * 
+ * @author Meara Patterson
+ * @version 3/29/2023, Version 1
+ */
+public class GUITest
+{
+  
+  public static void shoppingListViewerTest()
+  {
+    
+    ArrayList<Ingredient> ingredients = new ArrayList<>();
+    ingredients.add(new Ingredient("chicken", " ", 1, "g"));
+    ingredients.add(new Ingredient("rice", " ", 1, "g"));
+    ingredients.add(new Ingredient("broccoli", " ", 1, "g"));
+    
+    ArrayList<Utensil> utensils = new ArrayList<>();
+    utensils.add(new Utensil("fork", " "));
+    utensils.add(new Utensil("knife", " "));
+    
+    ArrayList<Step> steps = new ArrayList<>();
+    steps.add(new Step("cut", ingredients.get(0), utensils.get(0), utensils.get(1), " "));
+    
+    Recipe recipe = new Recipe("Shopping List Test", 2, ingredients, utensils, steps);
+    ShoppingListViewer shoppingList = new ShoppingListViewer(recipe);
+  }
+  
+  public static void main(String[] args)
+  {
+    shoppingListViewerTest();
+  }
+  
+}
