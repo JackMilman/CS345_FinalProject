fdf4d4c7ccf79871b8c98da17ebfad98c5aa9971
pegramay <refan@172.30.199.226>
2023-03-31 15:32:05 -0400

    NutritionInfo parse added.

--- src/recipes/NutritionInfo.java
+++ src/recipes/NutritionInfo.java
@@ -112,4 +112,16 @@ public enum NutritionInfo
     this.gramPerML = gramPerML;
   }
   
-}
+  public NutritionInfo fromCode(String ingredient) {
+    NutritionInfo[] info = NutritionInfo.values();
+    for (int i = 0; i < info.length; i++)
+    {
+      if (info[i].getName().equals(ingredient))
+      {
+        return info[i];
+      }
+    }
+    return null;
+  }
+  }
+  
