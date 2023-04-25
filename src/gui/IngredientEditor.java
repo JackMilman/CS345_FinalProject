4aef3820cd333f0026f029f99de607d3d227d55e
JackMilman12 <milmanjp@dukes.jmu.edu>
2023-04-25 18:23:19 -0400

    Scrubbed mention of Substitutes from IngredientEditor

--- src/gui/IngredientEditor.java
+++ src/gui/IngredientEditor.java
@@ -54,7 +54,6 @@ public class IngredientEditor extends JPanel
 
   private JComboBox<String> selectIngredient;
   private JComboBox<String> unitSelect;
-  private JComboBox<String> substituteSelect;
   private JTextField detailField;
   private JTextField amountField;
   private JButton makeNewIngredient;
@@ -62,7 +61,6 @@ public class IngredientEditor extends JPanel
   private JButton deleteButton;
   // private TextArea ingredientDisplay;
   private JTable ingredientDisplay;
-  private TextArea substituteDisplay;
 
   private final Recipe workingRecipe;
 
@@ -105,13 +103,6 @@ public class IngredientEditor extends JPanel
     amountField.addActionListener(addListener);
     unitSelect.addActionListener(addListener);
 
-    substituteSelect = new JComboBox<>();
-
-    // ingredientDisplay = new TextArea(0, 0);
-    substituteDisplay = new TextArea(0, 0);
-
-    substituteDisplay.setEditable(false);
-
     ingredientDisplay = new JTable(new DefaultTableModel(3, 1));
     updateIngredientDisplay();
 
@@ -131,7 +122,6 @@ public class IngredientEditor extends JPanel
     add(inputFields, BorderLayout.NORTH);
     add(deleteButton, BorderLayout.EAST);
     add(ingredientDisplay, BorderLayout.CENTER);
-    add(substituteDisplay, BorderLayout.SOUTH);
 
     setVisible(true);
     setOpaque(false);
@@ -185,7 +175,6 @@ public class IngredientEditor extends JPanel
     amountField.setText("");
 
     updateIngredientDisplay();
-    updateSubstituteArea();
   }
 
   private void delete()
@@ -224,55 +213,9 @@ public class IngredientEditor extends JPanel
 
   }
 
-  private void updateSubstituteArea()
-  {
-    String text = "";
-
-    Set<Ingredient> substitutableIngredients = workingRecipe.getSubstitutes().keySet();
-    for (Ingredient ingredient : substitutableIngredients)
-    {
-      List<Ingredient> substitutesForIngredient = workingRecipe.getSubstitutes().get(ingredient);
-      if (substitutesForIngredient != null)
-      {
-        for (Ingredient substitute : substitutesForIngredient)
-        {
-          text += String.format("Substitute %s for %s\n", substitute.toString(),
-              ingredient.toString());
-        }
-      }
-    }
-    substituteDisplay.setText(text);
-  }
-
-  /**
-   * updates the selectable "substitute" options. Should be called after loadIngredient is called.
-   */
-  private void updateSubstituteSelect()
-  {
-    substituteSelect.removeAllItems();
-
-    substituteSelect.addItem(BLANK);
-
-    for (Ingredient ingredient : workingRecipe.getIngredients())
-    {
-      substituteSelect.addItem(ingredient.getName());
-    }
-
-  }
-
   List<Ingredient> getIngredients()
   {
     return workingRecipe.getIngredients();
-  }
-
-  /**
-   * Gets the substitute Ingredients in the Recipe.
-   * 
-   * @return the substitute ingredients in the recipe.
-   */
-  HashMap<Ingredient, List<Ingredient>> getSubstitutes()
-  {
-    return workingRecipe.getSubstitutes();
   }
 
   /**
@@ -360,15 +303,6 @@ public class IngredientEditor extends JPanel
     workingRecipe.addAllIngredients(newIngredients);
 
     updateIngredientDisplay();
-    updateSubstituteSelect();
-  }
-
-  void loadSubstitutes(final HashMap<Ingredient, List<Ingredient>> newSubs)
-  {
-    workingRecipe.getSubstitutes().clear();
-    workingRecipe.addAllSubstitutes(newSubs);
-
-    updateSubstituteArea();
   }
 
   /**
