0499ada2b8fa42714d5c1bbd7763fd0de6f70758
mearapatterson <patte2mk@dukes.jmu.edu>
2023-04-25 18:33:42 -0400

    Fixed error in ProcessViewer, didn't check if it works properly

--- src/recipes/PriceInfo.java
+++ src/recipes/PriceInfo.java
@@ -129,7 +129,7 @@ public class PriceInfo
    * 
    * If the given name does not match any built-in ingredients, return 0.0.
    * 
-   * @param find the name of a built-in ingredient
+   * @param ingredientName find the name of a built-in ingredient
    * @return the ingredient's price per tablespoon or 0.0
    */
   public static Double getPricePerTablespoon(final String ingredientName)
